253ddcae32a8711ed4ad179c5d0eebb9dde2aac2
kyldex <raphael.giagnorio@gmail.com>
2022-06-13 22:22:11 +0200

    create router

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
 
 const { DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, CLUSTER_ADDRESS } = process.env;
 
@@ -35,25 +35,6 @@ app.use((req, res, next) => {
 // Handle requests with application/json Content-Type and set req.body
 app.use(express.json());
 
-app.post('/api/stuff', (req, res, next) => {
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'New thing has been stored' }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => res.status(200).json(thing))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
